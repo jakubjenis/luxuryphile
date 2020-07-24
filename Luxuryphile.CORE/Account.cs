46fef5459804426322a55474ab067064245be69b
jakub <jakub.jenis@conware.cz>
2020-07-24 20:48:53 +0200

    Add account and customer class

---
+++ Luxuryphile.CORE/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace Luxuryphile.CORE
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Currency { get; set; }
+        public string Number { get; set; }
+    }
+}
