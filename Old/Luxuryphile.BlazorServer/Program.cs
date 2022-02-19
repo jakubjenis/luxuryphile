f6c5a7c2b54f6f7d92d081f00c1e418ebe5b01f0
Jakub Jenis <jakubjenis@gmail.com>
2022-02-19 18:48:10 +0100

    Move all old stuff out

--- Old/Luxuryphile.BlazorServer/Program.cs
+++ Old/Luxuryphile.BlazorServer/Program.cs
@@ -1,8 +1,6 @@
-using Luxuryphile.CORE.Data;
 using Luxuryphile.CORE.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Luxuryphile.Web.Areas.Identity;
 using MudBlazor.Services;
 
@@ -10,11 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services
